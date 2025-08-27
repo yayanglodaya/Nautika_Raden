d9f50ac1605381498e48a8927e63f861fa0d549d
yayanglodaya <75561755+yayanglodaya@users.noreply.github.com>
2025-08-27 09:01:16 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,9 +3,9 @@ import { connect } from "cloudflare:sockets";
 // import { Buffer } from "node:buffer";
 
 // Variables
-const rootDomain = "radennautika.yayang-suargana.workers.dev"; // Ganti dengan domain utama kalian
+const rootDomain = "yayang-suargana.workers.dev"; // Ganti dengan domain utama kalian
 const serviceName = "radennautika"; // Ganti dengan nama workers kalian
-const apiKey = ""; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
+const apiKey = "92f17c4c3fd9d1a8310ed75784cd55a17515e"; // Ganti dengan Global API key kalian (https://dash.cloudflare.com/profile/api-tokens)
 const apiEmail = ""; // Ganti dengan email yang kalian gunakan
 const accountID = ""; // Ganti dengan Account ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
 const zoneID = ""; // Ganti dengan Zone ID kalian (https://dash.cloudflare.com -> Klik domain yang kalian gunakan)
